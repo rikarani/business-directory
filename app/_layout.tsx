12f32c7b8aa66bdce5f555a1d813329d6b659f3f
rikarani <46773591+rikarani@users.noreply.github.com>
2024-09-27 22:53:48 +0700

    Add font loading in RootLayout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,13 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 
 export default function RootLayout(): React.JSX.Element {
+  useFonts({
+    outfit: require("../assets/fonts/Outfit-Regular.ttf"),
+    "outfit-medium": require("../assets/fonts/Outfit-Medium.ttf"),
+    "outfit-bold": require("../assets/fonts/Outfit-Bold.ttf"),
+  });
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" />
