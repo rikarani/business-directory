486e5fdf6a1014e034d21e207432c030136daf2c
rikarani <46773591+rikarani@users.noreply.github.com>
2024-09-28 01:16:50 +0700

    Update font loading in RootLayout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,16 @@
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
 
-export default function RootLayout(): React.JSX.Element {
-  useFonts({
+export default function RootLayout(): React.JSX.Element | null {
+  const [loaded, error] = useFonts({
     outfit: require("../assets/fonts/Outfit-Regular.ttf"),
     "outfit-medium": require("../assets/fonts/Outfit-Medium.ttf"),
     "outfit-bold": require("../assets/fonts/Outfit-Bold.ttf"),
   });
+
+  if (!loaded && !error) {
+    return null;
+  }
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
