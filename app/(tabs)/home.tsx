9a3f3d46b65cd926e5dac972133a41515f58111a
rikarani <46773591+rikarani@users.noreply.github.com>
2024-10-01 01:11:37 +0700

    Refactor Home component to use Header component

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,9 +1,5 @@
-import { Text, View } from "react-native";
+import Header from "@/components/Home/Header";
 
 export default function Home(): React.JSX.Element {
-  return (
-    <View>
-      <Text style={{ fontSize: 54, fontFamily: "outfit-bold" }}>ini adalah view home</Text>
-    </View>
-  );
+  return <Header />;
 }
