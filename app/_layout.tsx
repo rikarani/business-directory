aba2f130424c064e64b1e6721960a5e7fdc853d5
rikarani <46773591+rikarani@users.noreply.github.com>
2024-09-30 13:19:37 +0700

    Refactor _layout.tsx and add LoginScreen component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
+import { type TokenCache } from "@/types/TokenCache";
+
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+
+import * as SecureStore from "expo-secure-store";
+
+import LoginScreen from "@/components/LoginScreen";
 
 export default function RootLayout(): React.JSX.Element | null {
   const [loaded, error] = useFonts({
@@ -12,9 +19,45 @@ export default function RootLayout(): React.JSX.Element | null {
     return null;
   }
 
+  const tokenCache: TokenCache = {
+    async getToken(key) {
+      try {
+        const item = await SecureStore.getItemAsync(key);
+
+        if (item) {
+          console.log(`${key} was used \n`);
+        } else {
+          console.log("No values stored under key: " + key);
+        }
+
+        return item;
+      } catch (err) {
+        console.error("SecureStore get item error : ", err);
+        await SecureStore.deleteItemAsync(key);
+
+        return null;
+      }
+    },
+
+    async saveToken(key, token) {
+      try {
+        return SecureStore.setItemAsync(key, token);
+      } catch (err) {
+        return;
+      }
+    },
+  };
+
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" />
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string}>
+      <SignedIn>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </SignedIn>
+      <SignedOut>
+        <LoginScreen />
+      </SignedOut>
+    </ClerkProvider>
   );
 }
