ae9f2f388920829f727443637b797fcfccd875b7
rikarani <46773591+rikarani@users.noreply.github.com>
2024-09-30 13:18:52 +0700

    Refactor color constants and add GRAY color

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,8 +1,3 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
 const tintColorLight = "#0a7ea4";
 const tintColorDark = "#fff";
 
@@ -24,4 +19,5 @@ export const Colors = {
     tabIconSelected: tintColorDark,
   },
   PRIMARY: "#7F57F1",
+  GRAY: "#8F8F8F",
 };
