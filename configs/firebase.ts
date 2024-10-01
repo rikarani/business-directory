044e590357cbc8988d098780fabaa0e869c49ec9
rikarani <46773591+rikarani@users.noreply.github.com>
2024-10-01 21:14:14 +0700

    Add firebase configuration file

---
+++ configs/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp, type FirebaseOptions } from "firebase/app";
+
+const config: FirebaseOptions = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+export const app = initializeApp(config);
