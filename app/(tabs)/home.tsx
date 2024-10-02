13575568e50eb8a8280684da6c27992215356ca9
rikarani <46773591+rikarani@users.noreply.github.com>
2024-10-02 13:20:09 +0700

    Refactor Home component to include Slider component

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,5 +1,13 @@
+import { View } from "react-native";
+
 import Header from "@/components/Home/Header";
+import Slider from "@/components/Home/Slider";
 
 export default function Home(): React.JSX.Element {
-  return <Header />;
+  return (
+    <View>
+      <Header />
+      <Slider />
+    </View>
+  );
 }
