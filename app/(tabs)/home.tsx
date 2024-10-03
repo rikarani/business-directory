ff5a400e4b1843847e109361e92071a7871998f0
rikarani <46773591+rikarani@users.noreply.github.com>
2024-10-03 11:32:45 +0700

    Add Category component to Home

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -2,12 +2,14 @@ import { View } from "react-native";
 
 import Header from "@/components/Home/Header";
 import Slider from "@/components/Home/Slider";
+import Category from "@/components/Home/Category";
 
 export default function Home(): React.JSX.Element {
   return (
     <View>
       <Header />
       <Slider />
+      <Category />
     </View>
   );
 }
