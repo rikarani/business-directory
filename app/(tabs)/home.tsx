6db59a366ae6f564cfa903fea2a4d8beb9757090
rikarani <46773591+rikarani@users.noreply.github.com>
2024-10-05 02:53:59 +0700

    Add PopularBusiness component to Home

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,15 +1,17 @@
-import { View } from "react-native";
+import { ScrollView } from "react-native";
 
 import Header from "@/components/Home/Header";
 import Slider from "@/components/Home/Slider";
 import Category from "@/components/Home/Category";
+import PopularBusiness from "@/components/Home/PopularBusiness";
 
 export default function Home(): React.JSX.Element {
   return (
-    <View>
+    <ScrollView>
       <Header />
       <Slider />
       <Category />
-    </View>
+      <PopularBusiness />
+    </ScrollView>
   );
 }
